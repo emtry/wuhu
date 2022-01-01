781200b6179eee045dbea1b983e4e7cf3ed8fae4
ehgenong <pyzlypz@gmail.com>
2022-01-01 10:16:22 +0800

    Update wuhu 1.1.1

--- utils/updater.bat
+++ utils/updater.bat
@@ -4,7 +4,7 @@ set pwd=%~dp0
 rmdir  /q/s "%pwd:~0,-7%\GreenLuma 2020"
 cls
 for /F %%i in (%pwd:~0,-7%\version.txt) do (set version=%%i)
-for /F %%j in ('curl https://raw.githubusercontent.com/emtry/wuhu/master/version.txt') do (set last=%%j)
+for /F %%j in ('curl https://cdn.jsdelivr.net/gh/emtry/wuhu@master/version.txt') do (set last=%%j)
 echo %last|findstr "404" >nul
 set status=%errorlevel%
 echo %last|findstr "1." >nul
@@ -19,7 +19,7 @@ else if %status% neq 1 (
 else if %flag% equ 1 (
  if %version% neq %last% (
   echo New release %last%
-  curl -o %pwd%tmp\wuhu_%last%.zip https://github.com/emtry/wuhu/archive/master.zip
+  curl -o %pwd%tmp\wuhu_%last%.zip https://shrill-pond-3e81.hunsh.workers.dev/https://github.com/emtry/wuhu/archive/master.zip
   unzip -o -d %pwd%tmp\ %pwd%tmp\wuhu_%last%.zip
   xcopy /s/e/y %pwd%tmp\wuhu-master\* %pwd:~0,-7%
   rmdir  /q/s %pwd%tmp\wuhu-master
