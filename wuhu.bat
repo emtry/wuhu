0b0e56bcbffe032bf733a76380ba1f6c73ee5d5a
ehgenong <pyzlypz@gmail.com>
2022-06-29 19:42:25 +0800

    Update wuhu 1.1.5

--- wuhu.bat
+++ wuhu.bat
@@ -1,6 +1,6 @@
 @echo off & setlocal enabledelayedexpansion
 
-start /d %~dp0utils\ %~dp0utils\updater.bat
+start cmd /C "%~dp0utils\updater.bat"
 
 if not exist "%~dp0utils\GreenLuma 2020\AppList" (
         mkdir "%~dp0utils\GreenLuma 2020\AppList\"
